ab4cf2a335853f73a977a37e58f0637dec220e4a
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-01-24 01:51:51 +0530

    creates separate file for main

--- sprint.js
+++ sprint.js
@@ -1,39 +1,14 @@
-import { commands } from "./commands.js";
+import { display, getInputCode, sprintRunner } from "./src/sprintLib.js";
 
-const getInputCode = () => {
-  const inputCode = prompt("Enter sprint code : ");
-  return inputCode.split(" ").map((e) => +e);
-};
-
-const getCodeCopy = (inputCode) => {
-  const codeCopy = Array.from(inputCode);
-  codeCopy.unshift(0);
-  return codeCopy;
-};
-
-const execute = (codeCopy, cellNumber) => {
-  const command = codeCopy[cellNumber];
-  if (command in commands) {
-    return commands[command]([codeCopy, cellNumber]);
+const main = () => {
+  try {
+    const code = getInputCode();
+    const { inputCode, executedCode } = sprintRunner(code);
+    display(inputCode, executedCode);
+  } catch (err) {
+    console.error(err);
+    Deno.exit(1);
   }
 };
 
-const display = (inputCode, outputCode) => {
-  console.log("\nINPUT\n", inputCode);
-  outputCode.shift();
-  console.log("\nOUTPUT\n", outputCode);
-};
-
-const start = () => {
-  const inputCode = getInputCode();
-  const codeCopy = getCodeCopy(inputCode);
-  let cellNumber = 1;
-
-  while (codeCopy[cellNumber] !== 9) {
-    cellNumber = execute(codeCopy, cellNumber);
-  }
-
-  display(inputCode, codeCopy);
-};
-
-start();
+main();
