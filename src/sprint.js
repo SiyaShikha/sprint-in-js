3a2e8f862d1f999cde17eda3dfb0c6f8669ed015
Shikha <150659967+SiyaShikha@users.noreply.github.com>
2025-01-31 21:06:06 +0530

    moved main sprint.js to source directory

---
+++ src/sprint.js
@@ -0,0 +1,14 @@
+import { display, getInputCode, sprintRunner } from "./sprintLib.js";
+
+const main = () => {
+  try {
+    const code = getInputCode();
+    const { inputCode, executedCode } = sprintRunner(code);
+    display(inputCode, executedCode);
+  } catch (err) {
+    console.error(err.msg);
+    Deno.exit(1);
+  }
+};
+
+main();
